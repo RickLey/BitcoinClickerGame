55eb8e2259cc6440eb0626b4f9fcb94ea05e4510
Rick Ley <rickwley@gmail.com>
2014-11-10 21:08:25 -0800

    Groundwork for Server

---
+++ BitcoinClickerGame/src/GamePackage/Server.java
@@ -0,0 +1,16 @@
+package GamePackage;
+
+import java.net.Socket;
+import java.util.HashMap;
+
+public class Server {
+	
+	HashMap<String, Socket> gameplaySockets;
+	HashMap<String, Socket> chatSockets;
+	HashSet<String> remainingPlayers;
+	
+
+	public static void main(String[] args) {
+	}
+
+}
