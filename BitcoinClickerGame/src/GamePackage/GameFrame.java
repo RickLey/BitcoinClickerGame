0d2e1e1c71b5038708e8df11ec4e6567cee1b6c6
sthsher <tsher@usc.edu>
2014-11-10 21:40:54 -0800

    SS: Started prototype for chat panel

--- BitcoinClickerGame/src/GamePackage/GameFrame.java
+++ BitcoinClickerGame/src/GamePackage/GameFrame.java
@@ -1,20 +1,89 @@
 package GamePackage;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class GameFrame extends JFrame{
+	
+	private JPanel chatPanel	= new JPanel();
+	
+	
 	public GameFrame(){
-		setSize(1000,700);
+		setSize(1200,700);
 		setLocation(100,200);
 		
+		setupChatPanel();
 		
-		
-		
+		add(chatPanel, BorderLayout.EAST);
 		
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
+	//Setup Functions
+	
+	//	********************* ChatPanel *************************
+	private JPanel		chatSouthPanel	= new JPanel();
+	
+	private JTextArea writingArea 		= new JTextArea();
+	private JTextArea chatArea			= new JTextArea();
+	private JLabel chatTitleLabel		= new JLabel("BitCoin Chat");
+	private JButton sendButton			= new JButton("Send");
+	
+	private JScrollPane chatScroller = new JScrollPane(chatArea, 
+														JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, 
+														JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+	private JScrollPane writeScroller = new JScrollPane(writingArea, 
+														JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, 
+														JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+	
+	
+	private void setupChatPanel(){
+		chatPanel.setPreferredSize(new Dimension(300,700));
+		chatPanel.setBackground(Color.WHITE);
+		
+		//Title
+		chatTitleLabel.setFont(new Font("Helvetica", Font.PLAIN, 18));
+		
+		//Setup scroller
+		chatArea.setBackground(Color.WHITE);
+		chatArea.setLineWrap(true);
+		
+		chatScroller.setPreferredSize(new Dimension(280, 500));
+		chatScroller.setBackground(Color.WHITE);
+		
+		//writing area
+		writingArea.setBackground(Color.WHITE);
+		writingArea.setLineWrap(true);
+		
+		writeScroller.setPreferredSize(new Dimension(200,125));
+		
+		//Button
+
+		
+		//South panel
+		chatSouthPanel.setBackground(Color.WHITE);
+		chatSouthPanel.add(writeScroller, BorderLayout.CENTER);
+		chatSouthPanel.add(sendButton, BorderLayout.EAST);
+		
+		
+		chatPanel.add(chatTitleLabel, BorderLayout.NORTH);
+		chatPanel.add(chatScroller, BorderLayout.CENTER);
+		chatPanel.add(chatSouthPanel, BorderLayout.SOUTH);
+		
+	}
+	
+	//	********************* ChatPanel *************************
+
 	
 	public static void main(String[] args){
 		new GameFrame();
