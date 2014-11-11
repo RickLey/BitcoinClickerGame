72870ab98f173e3ed3764b14d985088e6e635d59
Idean Keshmirian <idean.keshmirian@gmail.com>
2014-11-10 20:54:29 -0800

    I: Added more backend classes

---
+++ BitcoinClickerGame/src/GamePackage/IOHandler.java
@@ -0,0 +1,5 @@
+package GamePackage;
+
+public interface IOHandler {
+
+}
--- BitcoinClickerGame/src/GamePackage/Player.java
+++ BitcoinClickerGame/src/GamePackage/Player.java
@@ -1,5 +1,14 @@
 package GamePackage;
 
-public class Player {
-
+public class Player extends Thread {
+	private Store myStore;
+	private double coins;
+	private int health;
+	private ArrayList<String> opponentAliases;
+	private IOHandler threadHandler;
+	private Item currentSelectedItem;
+	
+	public setHandler(IOHandler replacement) {
+		threadHandler = replacement;
+	}
 }
