d815b4978fe32018a5e6afe6d327db5d784796d9
sthsher <tsher@usc.edu>
2014-11-11 19:37:50 -0800

    SS: Got chatpanel to work for self, added images directory

--- BitcoinClickerGame/src/GamePackage/GameFrame.java
+++ BitcoinClickerGame/src/GamePackage/GameFrame.java
@@ -4,7 +4,16 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -15,15 +24,17 @@ import javax.swing.JTextArea;
 public class GameFrame extends JFrame{
 	
 	private JPanel chatPanel	= new JPanel();
-	
+	private JPanel bitcoinPanel	= new JPanel();
 	
 	public GameFrame(){
 		setSize(1200,700);
 		setLocation(100,200);
 		
 		setupChatPanel();
+		setupBitcoinPanel();
 		
 		add(chatPanel, BorderLayout.EAST);
+		add(bitcoinPanel, BorderLayout.WEST);
 		
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -57,6 +68,7 @@ public class GameFrame extends JFrame{
 		//Setup scroller
 		chatArea.setBackground(Color.WHITE);
 		chatArea.setLineWrap(true);
+		chatArea.setEditable(false);
 		
 		chatScroller.setPreferredSize(new Dimension(280, 500));
 		chatScroller.setBackground(Color.WHITE);
@@ -64,11 +76,38 @@ public class GameFrame extends JFrame{
 		//writing area
 		writingArea.setBackground(Color.WHITE);
 		writingArea.setLineWrap(true);
+		writingArea.addKeyListener(new KeyAdapter(){
+			public void keyReleased(KeyEvent e){
+				if (e.getKeyCode() == KeyEvent.VK_ENTER){
+					if (!chatArea.getText().equals("")){
+						chatArea.setText(chatArea.getText() + writingArea.getText());
+					} else{
+						chatArea.setText(writingArea.getText());
+					}
+					
+					//Clear textarea
+					writingArea.setText("");
+					
+				}
+			}
+		});
 		
 		writeScroller.setPreferredSize(new Dimension(200,125));
 		
 		//Button
-
+		sendButton.addActionListener(new ActionListener(){
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (!chatArea.getText().equals("")){
+					chatArea.setText(chatArea.getText() + writingArea.getText() + "\n");
+				} else{
+					chatArea.setText(writingArea.getText() + "\n");
+				}
+				//Clear textarea
+				writingArea.setText("");
+			}
+		});
 		
 		//South panel
 		chatSouthPanel.setBackground(Color.WHITE);
@@ -82,8 +121,22 @@ public class GameFrame extends JFrame{
 		
 	}
 	
-	//	********************* ChatPanel *************************
+	//	********************* BitcoinPanel *************************
 
+	private JButton bitcoinButton = new JButton();
+	
+	private void setupBitcoinPanel(){
+		try{
+			BufferedImage bi = ImageIO.read(new File("images/bitcoin.jpg"));
+//			Image scaled = bi.getScaledInstance((, height, hints)
+		} catch (IOException IOE){
+			IOE.printStackTrace();
+		}
+		
+		
+		
+		bitcoinPanel.add(bitcoinButton, BorderLayout.CENTER);
+	}
 	
 	public static void main(String[] args){
 		new GameFrame();
