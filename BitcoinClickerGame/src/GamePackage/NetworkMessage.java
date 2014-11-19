6ca3843f7569e2c180879333e93f91b617afc14c
Dylan Davis <dylandav@usc.edu>
2014-11-18 22:12:58 -0800

    D: Created game class

---
+++ BitcoinClickerGame/src/GamePackage/NetworkMessage.java
@@ -0,0 +1,63 @@
+package GamePackage;
+
+import java.io.Serializable;
+
+public class NetworkMessage implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6672845399058436560L;
+	public static final String GAME_INITIALIZATION_MESSAGE = "Initialization Steps";
+	public static final String DRAW_WINDOW_MESSAGE = "Draw Window";
+	public static final String START_GAME_MESSAGE = "Start Game";
+	public static final String END_GAME_MESSAGE = "End Game";
+	public static final String UPDATE_MESSAGE = "Update";
+	public static final String ITEM_MESSAGE = "Item";
+	public static final String CHAT_MESSAGE = "Chat";
+	public static final String WHISPER_MESSAGE = "Whisper";
+	public static final String INITIALIZE_SERVER_MESSAGE = "Initialize Server";
+	public static final String BROADCAST = "All Users"; //for sending to all users
+	public static final String SERVER_ALIAS = "Server";
+	
+	String sender;
+	String recipient;
+	String messageType;
+	String itemType;
+	Object value;
+	
+	public NetworkMessage(){
+		
+	}
+	
+	public String getSender() {
+		return sender;
+	}
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	public String getRecipient() {
+		return recipient;
+	}
+	public void setRecipient(String recipient) {
+		this.recipient = recipient;
+	}
+	public String getMessageType() {
+		return messageType;
+	}
+	public void setMessageType(String messageType) {
+		this.messageType = messageType;
+	}
+	public String getItemType() {
+		return itemType;
+	}
+	public void setItemType(String itemType) {
+		this.itemType = itemType;
+	}
+	public Object getValue() {
+		return value;
+	}
+	public void setValue(Object value) {
+		this.value = value;
+	}
+}
