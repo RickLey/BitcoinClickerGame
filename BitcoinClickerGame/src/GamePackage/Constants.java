6ad319b5fa10414c9c376e4d11cb45b00918c9f7
sthsher <tsher@usc.edu>
2014-11-19 21:09:23 -0800

    SS: Modified constants

--- BitcoinClickerGame/src/GamePackage/Constants.java
+++ BitcoinClickerGame/src/GamePackage/Constants.java
@@ -5,7 +5,7 @@ import java.awt.Font;
 class Constants {
 	//Class for constants, a lot of them
 	
-	public static final int bitcoinButtonSize = 200;
+	public static final int bitcoinButtonSize = 250;
 	
 	public static Font getFont(int size){
 		return new Font("Helvetica", Font.PLAIN, size);
