9575a3e8a81cb8c2671f6359204d559d2b5a939d
Brian Tam <briantam@usc.edu>
2014-11-23 18:10:24 -0800

    Print statements when receiving items

--- BitcoinClickerGame/src/GamePackage/NullHandler.java
+++ BitcoinClickerGame/src/GamePackage/NullHandler.java
@@ -5,6 +5,12 @@ public class NullHandler implements IOHandler {
 	@Override
 
 	public void handleIncomingMessage(Game game, NetworkMessage nm) {
+		
+		if(nm.getMessageType().equals(NetworkMessage.ITEM_MESSAGE)) {
+			System.out.println("In handler");
+			System.out.println("Received: " + nm.getItemType());
+		}
+		
 		game.getLocalPlayer().receiveGameplayMessage(nm);
 	}
 
